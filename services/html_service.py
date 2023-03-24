0d55f113058612bcf79b03d9d9bc1fa70383a44e
TakWolf <takwolf@foxmail.com>
2023-03-24 21:11:33 +0800

    移除 html 压缩组件

--- services/html_service.py
+++ services/html_service.py
@@ -1,8 +1,6 @@
 import logging
 import os
 
-import minify_html
-
 import configs
 from configs import path_define, font_config
 from utils import fs_util
@@ -18,7 +16,6 @@ def make_alphabet_html_file(width_mode, alphabet):
         width_mode=width_mode,
         alphabet=''.join([c for c in alphabet if ord(c) >= 128]),
     )
-    html = minify_html.minify(html, minify_css=True, minify_js=True)
     fs_util.make_dirs_if_not_exists(path_define.outputs_dir)
     html_file_path = os.path.join(path_define.outputs_dir, font_config.get_alphabet_html_file_name(width_mode))
     with open(html_file_path, 'w', encoding='utf-8') as file:
@@ -32,7 +29,6 @@ def make_index_html_file():
         configs=configs,
         font_config=font_config,
     )
-    html = minify_html.minify(html, minify_css=True, minify_js=True)
     fs_util.make_dirs_if_not_exists(path_define.outputs_dir)
     html_file_path = os.path.join(path_define.outputs_dir, 'index.html')
     with open(html_file_path, 'w', encoding='utf-8') as file:
