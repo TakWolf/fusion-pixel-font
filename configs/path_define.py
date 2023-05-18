ca849d975935ef06ce968c96e2f285cd54c509cf
TakWolf <takwolf@foxmail.com>
2023-05-18 18:47:14 +0800

    清理无效路径

--- configs/path_define.py
+++ configs/path_define.py
@@ -3,7 +3,6 @@ import os
 project_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 assets_dir = os.path.join(project_root_dir, 'assets')
-unidata_dir = os.path.join(assets_dir, 'unidata')
 fonts_dir = os.path.join(assets_dir, 'fonts')
 templates_dir = os.path.join(assets_dir, 'templates')
 www_static_dir = os.path.join(assets_dir, 'www-static')
