03ee8643d7bd580f665946ddc3fd41207089b4b2
TakWolf <takwolf@foxmail.com>
2023-07-21 15:15:40 +0800

    统一 yaml 风格

--- configs/update_config.py
+++ configs/update_config.py
@@ -7,9 +7,9 @@ from configs import path_define
 
 class DownloadAssetConfig:
     def __init__(self, config_data: dict):
-        self.file_name: str = config_data['file-name']
+        self.file_name: str = config_data['file_name']
         self.copy_list = []
-        for from_path, to_path in config_data['copy-list'].items():
+        for from_path, to_path in config_data['copy_list'].items():
             if to_path is None:
                 to_path = from_path
             self.copy_list.append((from_path, to_path))
@@ -25,6 +25,6 @@ class UpdateConfig:
 
     def __init__(self, config_data: dict):
         self.name: str = config_data['name']
-        self.repository_name: str = config_data['repository-name']
-        self.tag_name: str | None = config_data['tag-name']
-        self.asset_configs = [DownloadAssetConfig(item_data) for item_data in config_data['asset-configs']]
+        self.repository_name: str = config_data['repository_name']
+        self.tag_name: str | None = config_data['tag_name']
+        self.asset_configs = [DownloadAssetConfig(item_data) for item_data in config_data['asset_configs']]
