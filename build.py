6551b24aaa572c1f8f7cd23440c58fb37814e159
TakWolf <takwolf@foxmail.com>
2024-03-17 06:14:53 +0800

    调整清理逻辑

--- build.py
+++ build.py
@@ -5,7 +5,10 @@ from scripts.utils import fs_util
 
 
 def main():
-    fs_util.delete_dir(path_define.build_dir)
+    fs_util.delete_dir(path_define.dump_dir)
+    fs_util.delete_dir(path_define.fallback_glyphs_dir)
+    fs_util.delete_dir(path_define.outputs_dir)
+    fs_util.delete_dir(path_define.releases_dir)
 
     update_service.setup_ark_pixel_glyphs()
 
