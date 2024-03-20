a23d30729935d66e18a385d199c12f2f3a433554
TakWolf <takwolf@foxmail.com>
2024-03-20 15:14:57 +0800

    调整缓存目录位置

--- scripts/configs/path_define.py
+++ scripts/configs/path_define.py
@@ -10,9 +10,8 @@ fonts_dir = os.path.join(assets_dir, 'fonts')
 templates_dir = os.path.join(assets_dir, 'templates')
 www_static_dir = os.path.join(assets_dir, 'www-static')
 
-cache_dir = os.path.join(project_root_dir, 'cache')
-
 build_dir = os.path.join(project_root_dir, 'build')
+cache_dir = os.path.join(build_dir, 'cache')
 dump_dir = os.path.join(build_dir, 'dump')
 fallback_glyphs_dir = os.path.join(build_dir, 'fallback')
 outputs_dir = os.path.join(build_dir, 'outputs')
