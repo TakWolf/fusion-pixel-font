93e609fff7f8e135315c3ffcf0ec1fdab60b53b1
TakWolf <takwolf@foxmail.com>
2024-06-03 02:04:36 +0800

    临时修复「美績点陣體」问题 https://github.com/ItMarki/MisekiBitmap/issues/3

--- scripts/tasks/update_fonts.py
+++ scripts/tasks/update_fonts.py
@@ -1,3 +1,6 @@
+from fontTools.ttLib import TTFont
+
+from scripts.configs import path_define
 from scripts.configs.update import UpdateConfig
 from scripts.services import update_service
 
@@ -9,6 +12,15 @@ def main():
     update_configs = UpdateConfig.load_all()
     for update_config in update_configs:
         update_service.update_fonts(update_config)
+
+    # TODO
+    # https://github.com/ItMarki/MisekiBitmap/issues/3
+    # 「美績点陣體」的位图层字形映射错乱，这里主动将其删除，以保证栅格化正确
+    font_file_path = path_define.fonts_dir.joinpath('miseki-bitmap', 'MisekiBitmap.ttf')
+    font = TTFont(font_file_path)
+    font.reader.tables.pop('EBLC', None)
+    font.reader.tables.pop('EBDT', None)
+    font.save(font_file_path)
 
 
 if __name__ == '__main__':
