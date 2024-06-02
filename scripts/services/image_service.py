5002deebd17243679960c5e347994f281ef50edf
TakWolf <takwolf@foxmail.com>
2024-06-02 14:37:09 +0800

    调整输出资源路径配置

--- scripts/services/image_service.py
+++ scripts/services/image_service.py
@@ -9,7 +9,7 @@ logger = logging.getLogger('image_service')
 
 
 def _load_font(font_config: FontConfig, width_mode: str, language_flavor: str, scale: int = 1) -> FreeTypeFont:
-    file_path = path_define.outputs_dir.joinpath(font_config.get_font_file_name(width_mode, language_flavor, 'woff2'))
+    file_path = path_define.outputs_dir.joinpath(f'fusion-pixel-{font_config.font_size}px-{width_mode}-{language_flavor}.woff2')
     return ImageFont.truetype(file_path, font_config.font_size * scale)
 
 
@@ -60,6 +60,6 @@ def make_preview_image_file(font_config: FontConfig):
     image = image.resize((image.width * 2, image.height * 2), Image.Resampling.NEAREST)
 
     path_define.outputs_dir.mkdir(parents=True, exist_ok=True)
-    file_path = path_define.outputs_dir.joinpath(font_config.preview_image_file_name)
+    file_path = path_define.outputs_dir.joinpath(f'preview-{font_config.font_size}px.png')
     image.save(file_path)
     logger.info("Make preview image file: '%s'", file_path)
