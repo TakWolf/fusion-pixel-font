a54f70b06c8d20b0415d71a66e3f5a33760cee8d
TakWolf <takwolf@foxmail.com>
2024-11-13 15:08:49 +0800

    优化 dump 脚本

--- tools/services/dump_service.py
+++ tools/services/dump_service.py
@@ -22,25 +22,23 @@ def dump_font(dump_config: DumpConfig):
     for code_point, glyph_name in font.getBestCmap().items():
         c = chr(code_point)
         block = unidata_blocks.get_block_by_code_point(code_point)
-        if not c.isprintable() and block.code_start != 0xE000:  # Private Use Area
+        if not c.isprintable() and block.name != 'Private Use Area':
             continue
 
         canvas_width = math.ceil(font['hmtx'].metrics[glyph_name][0] / font['head'].unitsPerEm * dump_config.rasterize_size)
         if canvas_width <= 0:
             continue
-        elif canvas_width > dump_config.font_size and block.code_start != 0xE000:  # Private Use Area
+        elif canvas_width > dump_config.font_size and block.name != 'Private Use Area':
             canvas_width = dump_config.font_size
 
         image = Image.new('RGBA', (canvas_width, canvas_height), (0, 0, 0, 0))
-        ImageDraw.Draw(image).text(dump_config.rasterize_offset, chr(code_point), fill=(0, 0, 0, 255), font=image_font)
+        ImageDraw.Draw(image).text(dump_config.rasterize_offset, c, fill=(0, 0, 0, 255), font=image_font)
 
-        block_dir_name = f'{block.code_start:04X}-{block.code_end:04X} {block.name}'
-        glyph_file_dir = dump_config.dump_dir.joinpath(block_dir_name)
-        hex_name = f'{code_point:04X}'
-        if block.code_start == 0x4E00:  # CJK Unified Ideographs
-            glyph_file_dir = glyph_file_dir.joinpath(f'{hex_name[0:-2]}-')
-        glyph_file_path = glyph_file_dir.joinpath(f'{hex_name}.png')
-
+        glyph_file_dir = dump_config.dump_dir.joinpath(f'{block.code_start:04X}-{block.code_end:04X} {block.name}')
+        code_name = f'{code_point:04X}'
+        if block.name == 'CJK Unified Ideographs':
+            glyph_file_dir = glyph_file_dir.joinpath(f'{code_name[0:-2]}-')
+        glyph_file_path = glyph_file_dir.joinpath(f'{code_name}.png')
         glyph_file_dir.mkdir(parents=True, exist_ok=True)
         image.save(glyph_file_path)
 
@@ -53,15 +51,14 @@ def apply_fallback(fallback_config: FallbackConfig):
             if not glyph_file_name.endswith('.png'):
                 continue
             glyph_file_path_from = glyph_file_dir_from.joinpath(glyph_file_name)
-            hex_name = glyph_file_path_from.stem
-            code_point = int(hex_name, 16)
+            code_name = glyph_file_path_from.stem
+            code_point = int(code_name, 16)
             block = unidata_blocks.get_block_by_code_point(code_point)
-            block_dir_name = f'{block.code_start:04X}-{block.code_end:04X} {block.name}'
-            glyph_file_dir_to = fallback_config.dir_to.joinpath(block_dir_name)
-            if block.code_start == 0x4E00:  # CJK Unified Ideographs
-                glyph_file_dir_to = glyph_file_dir_to.joinpath(f'{hex_name[0:-2]}-')
+            glyph_file_dir_to = fallback_config.dir_to.joinpath(f'{block.code_start:04X}-{block.code_end:04X} {block.name}')
+            if block.name == 'CJK Unified Ideographs':
+                glyph_file_dir_to = glyph_file_dir_to.joinpath(f'{code_name[0:-2]}-')
             if fallback_config.flavor is not None:
-                glyph_file_name = f'{hex_name} {fallback_config.flavor}.png'
+                glyph_file_name = f'{code_name} {fallback_config.flavor}.png'
             glyph_file_path_to = glyph_file_dir_to.joinpath(glyph_file_name)
             glyph_file_dir_to.mkdir(parents=True, exist_ok=True)
             shutil.copyfile(glyph_file_path_from, glyph_file_path_to)
