5611c1f8b0740a65db8561324e97aaa21b4aa81e
TakWolf <takwolf@foxmail.com>
2025-12-25 19:54:54 +0800

    2025.12.25

--- tools/configs/__init__.py
+++ tools/configs/__init__.py
@@ -7,7 +7,7 @@ from tools.configs.fallback import FallbackConfig
 from tools.configs.font import FontConfig
 from tools.configs.upgrade import UpgradeConfig
 
-version = '2025.12.11'
+version = '2025.12.25'
 
 font_configs = {font_size: FontConfig.load(font_size) for font_size in options.font_sizes}
 
