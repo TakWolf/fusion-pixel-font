84adee5e7c4356f7d8b3fee0cc503edfd296acee
TakWolf <takwolf@foxmail.com>
2026-03-23 15:13:38 +0800

    check 脚本不需要 setup

--- tools/check.py
+++ tools/check.py
@@ -1,10 +1,8 @@
 from tools.configs import options
-from tools.services import setup_service, check_service
+from tools.services import check_service
 
 
 def main():
-    setup_service.setup_ark_pixel()
-
     for font_size in options.font_sizes:
         check_service.check_glyphs(font_size)
 
