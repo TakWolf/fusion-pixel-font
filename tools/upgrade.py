dca869dcb31eb2a61f14390ba72cf1e1f6f2591d
TakWolf <takwolf@foxmail.com>
2026-07-20 16:37:01 +0800

    格式化字体许可证

--- tools/upgrade.py
+++ tools/upgrade.py
@@ -1,4 +1,5 @@
 from tools import configs
+from tools.configs import path_define
 from tools.services import upgrade_service
 
 
@@ -7,6 +8,14 @@ def main():
 
     for upgrade_config in configs.upgrade_configs:
         upgrade_service.upgrade_fonts(upgrade_config)
+
+    for parent_dir, dir_names, file_names in path_define.fonts_dir.walk():
+        for file_name in file_names:
+            if not file_name.endswith('.txt'):
+                continue
+            file_path = parent_dir.joinpath(file_name)
+            text = file_path.read_text('utf-8')
+            file_path.write_text(text, 'utf-8')
 
 
 if __name__ == '__main__':
