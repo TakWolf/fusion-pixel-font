af304dca35ff7c06836337f658c36caec6cf7820
TakWolf <takwolf@foxmail.com>
2026-09-24 20:08:46 +0800

    迁移构建命令

---
+++ tools/build_artwork.py
@@ -0,0 +1,14 @@
+from tools.config import options
+from tools.config.font import FontConfig
+from tools.services import image_service
+
+
+def main() -> None:
+    for font_size in options.FONT_SIZES:
+        font_config = FontConfig.load(font_size)
+
+        image_service.make_preview_image(font_config)
+
+
+if __name__ == '__main__':
+    main()
--- tools/config/options.py
+++ tools/config/options.py
@@ -45,12 +45,3 @@ type FontFormat = Literal[
     'pcf',
 ]
 FONT_FORMATS = list[FontFormat](get_args(FontFormat.__value__))
-
-type Attachment = Literal[
-    'release',
-    'info',
-    'alphabet',
-    'html',
-    'image',
-]
-ATTACHMENTS = list[Attachment](get_args(Attachment.__value__))
